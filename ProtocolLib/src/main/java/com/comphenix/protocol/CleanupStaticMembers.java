3357fd6c9c23958e2d5677b313c68d7b76b4664b
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-02-26 13:37:33 +0100

    Fix NPE in CleanupStaticMembers.
    Determine if this is really necessary. No point cleanup up after
    yourself in a sewer.

--- ProtocolLib/src/main/java/com/comphenix/protocol/CleanupStaticMembers.java
+++ ProtocolLib/src/main/java/com/comphenix/protocol/CleanupStaticMembers.java
@@ -27,6 +27,9 @@ import com.comphenix.protocol.error.ErrorReporter;
 import com.comphenix.protocol.events.ListeningWhitelist;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.injector.BukkitUnwrapper;
+import com.comphenix.protocol.injector.server.AbstractInputStreamLookup;
+import com.comphenix.protocol.injector.server.TemporaryPlayerFactory;
+import com.comphenix.protocol.injector.spigot.SpigotPacketInjector;
 import com.comphenix.protocol.reflect.FieldUtils;
 import com.comphenix.protocol.reflect.FuzzyReflection;
 import com.comphenix.protocol.reflect.MethodUtils;
@@ -36,9 +39,11 @@ import com.comphenix.protocol.reflect.compiler.StructureCompiler;
 import com.comphenix.protocol.reflect.instances.CollectionGenerator;
 import com.comphenix.protocol.reflect.instances.DefaultInstances;
 import com.comphenix.protocol.reflect.instances.PrimitiveGenerator;
+import com.comphenix.protocol.utility.MinecraftReflection;
 import com.comphenix.protocol.wrappers.ChunkPosition;
 import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import com.comphenix.protocol.wrappers.WrappedWatchableObject;
+import com.comphenix.protocol.wrappers.nbt.io.NbtBinarySerializer;
 
 /**
  * Used to fix ClassLoader leaks that may lead to filling up the permanent generation.
@@ -66,7 +71,9 @@ class CleanupStaticMembers {
 				PrimitiveGenerator.class, FuzzyReflection.class, MethodUtils.class, 
 				BackgroundCompiler.class, StructureCompiler.class,
 				ObjectWriter.class, Packets.Server.class, Packets.Client.class, 
-				ChunkPosition.class, WrappedDataWatcher.class, WrappedWatchableObject.class
+				ChunkPosition.class, WrappedDataWatcher.class, WrappedWatchableObject.class,
+				AbstractInputStreamLookup.class, TemporaryPlayerFactory.class, SpigotPacketInjector.class,
+				MinecraftReflection.class, NbtBinarySerializer.class
 		};
 							   			
 		String[] internalClasses = {
@@ -76,14 +83,14 @@ class CleanupStaticMembers {
 			 "com.comphenix.protocol.injector.player.NetworkObjectInjector",
 			 "com.comphenix.protocol.injector.player.NetworkServerInjector",
 			 "com.comphenix.protocol.injector.player.PlayerInjector",
-			 "com.comphenix.protocol.injector.player.TemporaryPlayerFactory",
 			 "com.comphenix.protocol.injector.EntityUtilities",
 			 "com.comphenix.protocol.injector.packet.PacketRegistry",
 			 "com.comphenix.protocol.injector.packet.PacketInjector",
 			 "com.comphenix.protocol.injector.packet.ReadPacketModifier",
 			 "com.comphenix.protocol.injector.StructureCache",
 			 "com.comphenix.protocol.reflect.compiler.BoxingHelper",
-			 "com.comphenix.protocol.reflect.compiler.MethodDescriptor"
+			 "com.comphenix.protocol.reflect.compiler.MethodDescriptor",
+			 "com.comphenix.protocol.wrappers.nbt.WrappedElement",
 		};
 		
 		resetClasses(publicClasses);
