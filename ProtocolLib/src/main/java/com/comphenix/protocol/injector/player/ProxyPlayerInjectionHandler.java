8f2935e24171195b07ccb4482c9ec0d9b2f4365b
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-06-19 02:07:12 +0200

    Minimize the number of unnecessary references.

--- ProtocolLib/src/main/java/com/comphenix/protocol/injector/player/ProxyPlayerInjectionHandler.java
+++ ProtocolLib/src/main/java/com/comphenix/protocol/injector/player/ProxyPlayerInjectionHandler.java
@@ -18,6 +18,7 @@
 package com.comphenix.protocol.injector.player;
 
 import java.io.DataInputStream;
+import java.lang.ref.WeakReference;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -81,7 +82,7 @@ class ProxyPlayerInjectionHandler implements PlayerInjectionHandler {
 	private NetLoginInjector netLoginInjector;
 	
 	// The last successful player hook
-	private PlayerInjector lastSuccessfulHook;
+	private WeakReference<PlayerInjector> lastSuccessfulHook;
 	
 	// Dummy injection
 	private Cache<Player, PlayerInjector> dummyInjectors = 
@@ -399,7 +400,7 @@ class ProxyPlayerInjectionHandler implements PlayerInjectionHandler {
 			
 			// Update values
 			if (injector != null)
-				lastSuccessfulHook = injector;
+				lastSuccessfulHook = new WeakReference<PlayerInjector>(injector);
 			if (permanentHook != getPlayerHook(phase)) 
 				setPlayerHook(phase, tempHook);
 			
@@ -649,11 +650,21 @@ class ProxyPlayerInjectionHandler implements PlayerInjectionHandler {
 	@Override
 	public void checkListener(Set<PacketListener> listeners) {
 		// Make sure the current listeners are compatible
-		if (lastSuccessfulHook != null) {
+		if (getLastSuccessfulHook() != null) {
 			for (PacketListener listener : listeners) {
 				checkListener(listener);
 			}
 		}
+	}
+	
+	/**
+	 * Retrieve the last successful hook.
+	 * <p>
+	 * May be NULL if the hook has been uninjected.
+	 * @return Last successful hook.
+	 */
+	private PlayerInjector getLastSuccessfulHook() {
+		return lastSuccessfulHook != null ? lastSuccessfulHook.get() : null;
 	}
 	
 	/**
@@ -664,8 +675,10 @@ class ProxyPlayerInjectionHandler implements PlayerInjectionHandler {
 	 */
 	@Override
 	public void checkListener(PacketListener listener) {
-		if (lastSuccessfulHook != null) {
-			UnsupportedListener result = lastSuccessfulHook.checkListener(version, listener);
+		PlayerInjector last = getLastSuccessfulHook();
+		
+		if (last != null) {
+			UnsupportedListener result = last.checkListener(version, listener);
 
 			// We won't prevent the listener, as it may still have valid packets
 			if (result != null) {
