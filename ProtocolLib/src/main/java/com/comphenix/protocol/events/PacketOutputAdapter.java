8f30199a22f1228bf3442b5e2e28ae16d3ff20fb
Kristian S. Stangeland <kr_stang@hotmail.com>
2013-07-17 05:51:22 +0200

    Adding a packet handler adapter, for symmetri with PacketAdapter.

---
+++ ProtocolLib/src/main/java/com/comphenix/protocol/events/PacketOutputAdapter.java
@@ -0,0 +1,33 @@
+package com.comphenix.protocol.events;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Represents an adapter version of the output handler interface.
+ * 
+ * @author Kristian
+ */
+public abstract class PacketOutputAdapter implements PacketOutputHandler {
+	private final Plugin plugin;
+	private final ListenerPriority priority;
+	
+	/**
+	 * Construct a new packet output adapter with the given values.
+	 * @param priority - the output handler priority.
+	 * @param plugin - the owner plugin.
+	 */
+	public PacketOutputAdapter(Plugin plugin, ListenerPriority priority) {
+		this.priority = priority;
+		this.plugin = plugin;
+	}
+
+	@Override
+	public Plugin getPlugin() {
+		return plugin;
+	}
+	
+	@Override
+	public ListenerPriority getPriority() {
+		return priority;
+	}
+}
