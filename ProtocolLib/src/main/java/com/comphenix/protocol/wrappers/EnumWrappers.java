5efc4ffee367a40b670e81623717aa0602a8e27a
Dan Mulloy <dmulloy2@live.com>
2016-03-04 17:32:43 -0500

    Update player actions for 1.9
    Fixes #161

--- ProtocolLib/src/main/java/com/comphenix/protocol/wrappers/EnumWrappers.java
+++ ProtocolLib/src/main/java/com/comphenix/protocol/wrappers/EnumWrappers.java
@@ -158,8 +158,10 @@ public abstract class EnumWrappers {
 		STOP_SLEEPING,
 		START_SPRINTING,
 		STOP_SPRINTING,
-		RIDING_JUMP,
-		OPEN_INVENTORY;
+		START_RIDING_JUMP,
+		STOP_RIDING_JUMP,
+		OPEN_INVENTORY,
+		START_FALL_FLYING;
 	}
 
 	public enum ScoreboardAction {
