688da1a1bbe28bd3952f1eba5a50fc361d4b6742
Dan Mulloy <dmulloy2@live.com>
2016-04-03 00:49:04 -0400

    Add protocol version for 1.9.2

--- modules/API/src/main/java/com/comphenix/protocol/utility/MinecraftProtocolVersion.java
+++ modules/API/src/main/java/com/comphenix/protocol/utility/MinecraftProtocolVersion.java
@@ -40,6 +40,7 @@ public class MinecraftProtocolVersion {
 		map.put(new MinecraftVersion(1, 7, 6), 5);
 		map.put(new MinecraftVersion(1, 8, 0), 47);
 		map.put(new MinecraftVersion(1, 9, 0), 107);
+		map.put(new MinecraftVersion(1, 9, 2), 109);
 		return map;
 	}
 
