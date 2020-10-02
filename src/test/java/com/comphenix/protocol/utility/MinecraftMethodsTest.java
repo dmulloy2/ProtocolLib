e813be263bc575ec2bac95981177c9477c10e245
Pim van der Loos <pimvanderloos@gmail.com>
2020-10-03 01:58:45 +0200

    Unit test MinecraftMethods#initializePacket()

--- src/test/java/com/comphenix/protocol/utility/MinecraftMethodsTest.java
+++ src/test/java/com/comphenix/protocol/utility/MinecraftMethodsTest.java
@@ -1,11 +1,16 @@
 package com.comphenix.protocol.utility;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
+import com.comphenix.protocol.injector.server.TemporaryPlayer;
+import com.comphenix.protocol.injector.server.TemporaryPlayerFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.comphenix.protocol.BukkitInitialization;
+
+import java.lang.reflect.Field;
 
 public class MinecraftMethodsTest {
 
@@ -18,5 +23,20 @@ public class MinecraftMethodsTest {
 	public void testSendPacketMethods() {
 		assertNotNull(MinecraftMethods.getSendPacketMethod());
 		assertNotNull(MinecraftMethods.getNetworkManagerHandleMethod());
+	}
+
+	private void setNull(final String fieldName) throws NoSuchFieldException, IllegalAccessException {
+		Field field = MinecraftMethods.class.getDeclaredField(fieldName);
+		field.setAccessible(true);
+		field.set(null, null);
+	}
+
+	@Test
+	public void initializePacket() throws NoSuchFieldException, IllegalAccessException {
+		setNull("packetReadByteBuf");
+		setNull("packetWriteByteBuf");
+
+		assertNotNull(MinecraftMethods.getPacketWriteByteBufMethod());
+		assertNotNull(MinecraftMethods.getPacketReadByteBufMethod());
 	}
 }
