f11c24627672ccb1cd66582ab53e0dddb5d84391
Dan Mulloy <dev@dmulloy2.net>
2021-06-04 18:24:30 -0400

    Deprecate SocketInjector#getSocket (see #1188)

--- src/main/java/com/comphenix/protocol/injector/server/SocketInjector.java
+++ src/main/java/com/comphenix/protocol/injector/server/SocketInjector.java
@@ -18,22 +18,24 @@ public interface SocketInjector {
 	 * Retrieve the associated socket of this player.
 	 * @return The associated socket.
 	 * @throws IllegalAccessException If we're unable to read the socket field.
+	 * @deprecated May be null on certain server implementations. Also don't use raw sockets.
 	 */
-	public abstract Socket getSocket() throws IllegalAccessException;
+	@Deprecated
+	Socket getSocket() throws IllegalAccessException;
 
 	/**
 	 * Retrieve the associated address of this player.
 	 * @return The associated address.
 	 * @throws IllegalAccessException If we're unable to read the socket field.
 	 */
-	public abstract SocketAddress getAddress() throws IllegalAccessException;
+	SocketAddress getAddress() throws IllegalAccessException;
 
 	/**
 	 * Attempt to disconnect the current client.
 	 * @param message - the message to display.
 	 * @throws InvocationTargetException If disconnection failed.
 	 */
-	public abstract void disconnect(String message) throws InvocationTargetException;
+	void disconnect(String message) throws InvocationTargetException;
 
 	/**
 	 * Send a packet to the client.
@@ -42,36 +44,36 @@ public interface SocketInjector {
 	 * @param filtered - whether or not the packet will be filtered by our listeners.
 	 * @throws InvocationTargetException If an error occured when sending the packet.
 	 */
-	public abstract void sendServerPacket(Object packet, NetworkMarker marker, boolean filtered)
+	void sendServerPacket(Object packet, NetworkMarker marker, boolean filtered)
 			throws InvocationTargetException;
 
 	/**
 	 * Retrieve the hooked player.
 	 * @return The hooked player.
 	 */
-	public abstract Player getPlayer();
+	Player getPlayer();
 
 	/**
 	 * Retrieve the hooked player object OR the more up-to-date player instance.
 	 * @return The hooked player, or a more up-to-date instance.
 	 */
-	public abstract Player getUpdatedPlayer();
+	Player getUpdatedPlayer();
 
 	/**
 	 * Invoked when a delegated socket injector transfers the state of one injector to the next.
 	 * @param delegate - the new injector.
 	 */
-	public abstract void transferState(SocketInjector delegate);
+	void transferState(SocketInjector delegate);
 
 	/**
 	 * Set the real Bukkit player that we will use.
 	 * @param updatedPlayer - the real Bukkit player.
 	 */
-	public abstract void setUpdatedPlayer(Player updatedPlayer);
+	void setUpdatedPlayer(Player updatedPlayer);
 
 	/**
 	 * Determines if the player is currently connected.
 	 * @return true if the player is connected.
 	 */
-	public abstract boolean isConnected();
+	boolean isConnected();
 }
