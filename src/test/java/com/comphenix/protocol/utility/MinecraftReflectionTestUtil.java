a24695a76b70286c21d3e80ccf7ff7768fbb6770
derklaro <git@derklaro.dev>
2022-07-27 19:31:55 +0200

    some small pre-spigot-release updates

--- src/test/java/com/comphenix/protocol/utility/MinecraftReflectionTestUtil.java
+++ src/test/java/com/comphenix/protocol/utility/MinecraftReflectionTestUtil.java
@@ -5,10 +5,9 @@ public class MinecraftReflectionTestUtil {
 	public static final String PACKAGE_VERSION = "v1_19_R1";
 	public static final String NMS = "net.minecraft";
 	public static final String OBC = "org.bukkit.craftbukkit." + PACKAGE_VERSION;
-	public static final MinecraftVersion CURRENT_VERSION = MinecraftVersion.WILD_UPDATE;
 
 	public static void init() {
 		MinecraftReflection.setMinecraftPackage(NMS, OBC);
-		MinecraftVersion.setCurrentVersion(CURRENT_VERSION);
+		MinecraftVersion.setCurrentVersion(MinecraftVersion.LATEST);
 	}
 }
