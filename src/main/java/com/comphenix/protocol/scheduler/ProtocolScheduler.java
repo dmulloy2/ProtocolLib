d4b4f50674bc8ea8ede61181e8070fa9ef6bee48
Maurice Eisenblätter <Ingrim4@users.noreply.github.com>
2023-10-25 03:10:35 +0200

    Fix async handler scheduler support for folia (#2531)

--- src/main/java/com/comphenix/protocol/scheduler/ProtocolScheduler.java
+++ src/main/java/com/comphenix/protocol/scheduler/ProtocolScheduler.java
@@ -1,12 +1,11 @@
 package com.comphenix.protocol.scheduler;
 
-import com.comphenix.protocol.ProtocolLib;
-import org.bukkit.plugin.Plugin;
-
 public interface ProtocolScheduler {
     Task scheduleSyncRepeatingTask(Runnable task, long delay, long period);
 
     Task runTask(Runnable task);
 
     Task scheduleSyncDelayedTask(Runnable task, long delay);
+
+    Task runTaskAsync(Runnable task);
 }
