c2d45c52d4837c1d1294d15cf00e00b9a5d393a6
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2024-05-17 15:52:21 +0300

    Add even more methods to work with

--- src/main/java/com/comphenix/protocol/wrappers/AdventureComponentConverter.java
+++ src/main/java/com/comphenix/protocol/wrappers/AdventureComponentConverter.java
@@ -39,12 +39,30 @@ public class AdventureComponentConverter {
     }
 
     /**
+     * Converts a {@link WrappedChatComponent} into a {@link Component}
+     * @param wrapper ProtocolLib wrapper
+     * @return Component
+     */
+    public static Object fromWrapperAsObject(WrappedChatComponent wrapper) {
+            return fromWrapper(wrapper);
+    }
+
+    /**
+     * Converts a JSON {@link String} into a Component
+     * @param json Json String
+     * @return Component
+     */
+    public static Component fromJson(final String json) {
+            return GsonComponentSerializer.gson().deserialize(json);
+    }
+
+    /**
      * Converts a JSON {@link String} into a Component object
      * @param json Json String
      * @return Component object
      */
-    public static Object fromJson(final String json) {
-        return GsonComponentSerializer.gson().deserialize(json);
+    public static Object fromJsonAsObject(final String json) {
+            return fromJson(json);
     }
 
     /**
