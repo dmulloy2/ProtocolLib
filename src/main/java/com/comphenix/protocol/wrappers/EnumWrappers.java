9417eee444f64e0960b27cf36c07cfe747acc63a
Pasqual Koschmieder <git@derklaro.dev>
2025-06-19 19:57:42 +0200

    drop unnecessary minecraft import from EnumWrappers (#3476)

--- src/main/java/com/comphenix/protocol/wrappers/EnumWrappers.java
+++ src/main/java/com/comphenix/protocol/wrappers/EnumWrappers.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import net.minecraft.world.entity.Interaction;
 import org.apache.commons.lang.Validate;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
